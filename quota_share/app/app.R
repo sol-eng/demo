dd9564c9a973421d462919d932be0104611ebcd7
kevinykuo <kevinykuo@gmail.com>
2020-07-09 01:33:46 +0800

    remove old comments

--- quota_share/app/app.R
+++ quota_share/app/app.R
@@ -1,22 +1,10 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(xs)
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  # Application title
   titlePanel("Quota Share Example"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       sliderInput("ceded",
@@ -36,14 +24,12 @@ ui <- fluidPage(
                    max = NA)
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
       verbatimTextOutput("analysis")
     )
   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   
   output$analysis <- renderText({
@@ -57,5 +43,4 @@ server <- function(input, output) {
   }, sep = "\n")
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
